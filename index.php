7d9e36e8591f6816185facab9405bf7319a121ae
Rodrigo Martins <63435728+Rodriggodev@users.noreply.github.com>
2020-04-10 18:47:22 -0300

    index

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php require "../app/function/pages.php";
+//chamando o arquivo
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="UTF-8">
+	<title>curso de php</title>
+	<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css">
+
+</head>
+<body>
+	<div class="container">
+		<?php
+			require load()//load é a function em outro arquivo; 
+		 ?>
+	</div>
+</body>
+</html>
