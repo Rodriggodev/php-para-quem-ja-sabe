412136a1829986d33fa2cd3bde8b50138e502354
Rodrigo Martins <63435728+Rodriggodev@users.noreply.github.com>
2020-04-15 22:30:16 -0300

    elemte.js
    elementos no js

---
+++ element.js
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>curso de js</title>
+</head>
+<body>
+
+	<div id="app">
+		<input type="text" name="nome">
+		<button class="botao">adicionar</button>
+	</div>
+	<a href="https://github.com/Rodriggodev/php-para-quem-ja-sabe/tree/master">guithub</a>
+	<script>
+		
+		var linkElement.document.createElement('a');//ta a para linkar do html
+
+		linkElement.setAttribute('href', 'https://github.com/Rodriggodev/php-para-quem-ja-sabe/tree/master');
+		
+		//link com o setAttribute
+
+		var textElemnte = document.createTextNode('acesar guithub');
+		textElemnte.appendChild(text);
+
+		//aqui estamos linkando o texto ao link
+
+
+	</script>
+</body>
